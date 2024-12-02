fa598fc8ae30d3ad2a33d2fd446eb1c65197e2fc
Florian Bachmann <f.bachmann@lvm.de>
2024-12-02 08:50:26 +0100

    Adds builds

---
+++ build/esm/parse-markdown.js
@@ -0,0 +1,39 @@
+function parseMarkdown(markdown) {
+  if (!markdown) {
+    throw new Error("data is null, undefined or empty");
+  }
+  if (typeof markdown !== "string") {
+    throw new Error("data is not a string");
+  }
+  markdown = markdown.trim();
+  if (markdown === "") {
+    throw new Error("data is an empty string");
+  }
+  const headings = [...markdown.matchAll(/^#{1,6} (?<title>.+)/gm)].map(({ 0: heading, 1: title, index }) => ({
+    heading,
+    title,
+    index
+  }));
+  if (headings.length > 0 && headings[0].index > 0) {
+    headings.unshift({
+      heading: "",
+      title: "raw",
+      index: 0
+    });
+  }
+  const parsed = {};
+  for (let i = 0; i < headings.length; i++) {
+    parsed[headings[i].title] = {
+      heading: headings[i].heading,
+      content: markdown.slice(
+        headings[i].index + headings[i].heading.length + 1,
+        i < headings.length - 1 ? headings[i + 1].index : void 0
+      ).trim()
+    };
+  }
+  return parsed;
+}
+;
+export {
+  parseMarkdown as default
+};
